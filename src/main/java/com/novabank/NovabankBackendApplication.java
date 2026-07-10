6ae36333e91e7afc5f790d9d0da6a4837c8cc8cb
Abhay Raj <abhayraj83843@gmail.com>
2026-07-10 16:57:46 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/novabank/NovabankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.novabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovabankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovabankBackendApplication.class, args);
+    }
+
+}
